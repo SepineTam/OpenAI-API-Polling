63a06f8da97e2629f113acad0a528007d51e54ec
SongTan <sepinetam@gmail.com>
2026-01-03 13:01:26 +0800

    lint

--- src/openai_api_polling/polling/__init__.py
+++ src/openai_api_polling/polling/__init__.py
@@ -1,5 +1,5 @@
 from .api_polling import APIPolling
-from .client_polling import ClientPolling, ClaudeClientPolling, GeminiClientPolling
+from .client_polling import ClaudeClientPolling, ClientPolling, GeminiClientPolling
 
 __all__ = [
     "APIPolling",
